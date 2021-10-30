84bfb8df9754e6536e4f06efb9b49b3ceafacf1b
Vlad M. Cranga <vladcranga@yahoo.com>
2021-10-30 18:10:52 +0100

    format the code using black

--- weather.py
+++ weather.py
@@ -1,8 +1,8 @@
-'''
+"""
 weather.py
 An app that displays the weather in a location.
 It uses the openweathermap API: https: / openweathermap.org/
-'''
+"""
 
 import PySimpleGUI as sg
 import requests
@@ -14,55 +14,32 @@ fhand = open(r"api.txt", "r")
 api_key = fhand.read()
 
 # specify the theme
-sg.theme('BlueMono')
+sg.theme("BlueMono")
 
 # list the layout elements
 left_half = [
-
     [
         sg.Text("Enter the latitude: "),
         sg.In(size=(25, 1), enable_events=True, key="lat"),
     ],
-
     [
         sg.Text("Enter the longitude: "),
-        sg.In(size=(25, 1), enable_events=True, key="long")
+        sg.In(size=(25, 1), enable_events=True, key="long"),
     ],
-
-    [
-        sg.Button("Show"),
-        sg.Button("Get Coordinates"),
-        sg.Button("Quit")
-    ],
+    [sg.Button("Show"), sg.Button("Get Coordinates"), sg.Button("Quit")],
 ]
 
 right_half = [
-
-    [
-        sg.Text("The weather forecast is: ", pad=(145, 0))
-    ],
-
-    [
-        sg.Text("", size=(55, 1), key="temperature")
-    ],
-
-    [
-        sg.Text("", size=(60, 1), key="temperature_tomorrow")
-    ],
+    [sg.Text("The weather forecast is: ", pad=(145, 0))],
+    [sg.Text("", size=(55, 1), key="temperature")],
+    [sg.Text("", size=(60, 1), key="temperature_tomorrow")],
 ]
 
-layout = [
-
-    [
-        sg.Column(left_half),
-        sg.VSeparator(),
-        sg.Column(right_half)
-    ]
-]
+layout = [[sg.Column(left_half), sg.VSeparator(), sg.Column(right_half)]]
 
 #  create the window
-window = sg.Window("Weather Forecast", layout,
-                   margins=(10, 10), size=(860, 110))
+window = sg.Window("Weather Forecast", layout, margins=(10, 10),
+                   size=(860, 110))
 
 # the event loop
 while True:
@@ -70,34 +47,44 @@ while True:
 
     # display weather information
     if event == "Show":
-        lat = values['lat']
-        long = values['long']
+        lat = values["lat"]
+        long = values["long"]
 
-        url = "https://api.openweathermap.org/data/2.5/onecall" + \
-            "?lat=%s&lon=%s&appid=%s&units=metric" % (lat, long, api_key)
+        url = (
+            "https://api.openweathermap.org/data/2.5/onecall"
+            + "?lat=%s&lon=%s&appid=%s&units=metric" % (lat, long, api_key)
+        )
 
         response = requests.get(url)
         data = json.loads(response.text)
 
         try:
-            temperature = "The current temperature in " + \
-                data['timezone'] + " is " + str(data['current']['temp']) + \
-                " degrees Celsius."
+            temperature = (
+                "The current temperature in "
+                + data["timezone"]
+                + " is "
+                + str(data["current"]["temp"])
+                + " degrees Celsius."
+            )
 
-            temperature_tomorrow = "The temperature tomorrow will be " + \
-                str(data['daily'][1]['temp']['day']) + " during the day and " + \
-                str(data['daily'][1]['temp']['night']) + " during the night."
+            temperature_tomorrow = (
+                "The temperature tomorrow will be "
+                + str(data["daily"][1]["temp"]["day"])
+                + " during the day and "
+                + str(data["daily"][1]["temp"]["night"])
+                + " during the night."
+            )
 
         except Exception:
             temperature = "Could not retrieve weather data."
             temperature_tomorrow = "Could not retrieve future weather data."
 
-        window['temperature'].update(temperature)
-        window['temperature_tomorrow'].update(temperature_tomorrow)
+        window["temperature"].update(temperature)
+        window["temperature_tomorrow"].update(temperature_tomorrow)
 
     # help the user get coordinates for their location
     if event == "Get Coordinates":
-        webbrowser.open('https://latlong.net')
+        webbrowser.open("https://latlong.net")
 
     # close the window
     if event == "Quit" or event == sg.WIN_CLOSED:
