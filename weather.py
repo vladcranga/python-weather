57f2ca98b41221dcd5ff78ccb61a72521becdd8e
Vlad Mihai Cranga <vmcranga1@sheffield.ac.uk>
2024-08-15 00:56:21 +0300

    update api usage

--- weather.py
+++ weather.py
@@ -1,8 +1,7 @@
-"""
-weather.py
-An app that displays the weather in a location.
-It uses the openweathermap API: https: / openweathermap.org/
-"""
+# weather.py
+# An app that displays the weather in a location.
+# It uses the openweathermap API: https: / openweathermap.org/
+
 
 import PySimpleGUI as sg
 import requests
@@ -10,7 +9,7 @@ import json
 import webbrowser
 
 # the API key
-fhand = open(r"api.txt", "r")
+fhand = open("api.txt", "r")
 api_key = fhand.read()
 
 # specify the theme
@@ -30,16 +29,15 @@ left_half = [
 ]
 
 right_half = [
-    [sg.Text("The weather forecast is: ", pad=(145, 0))],
-    [sg.Text("", size=(55, 1), key="temperature")],
-    [sg.Text("", size=(60, 1), key="temperature_tomorrow")],
+    [sg.Text("Weather Information: ", pad=(145, 0))],
+    [sg.Text("", size=(70, 2), key="weather_info")],
 ]
 
 layout = [[sg.Column(left_half), sg.VSeparator(), sg.Column(right_half)]]
 
 #  create the window
 window = sg.Window("Weather Forecast", layout, margins=(10, 10),
-                   size=(860, 110))
+                   size=(860, 130))
 
 # the event loop
 while True:
@@ -49,38 +47,25 @@ while True:
     if event == "Show":
         lat = values["lat"]
         long = values["long"]
-
-        url = (
-            "https://api.openweathermap.org/data/2.5/onecall"
-            + "?lat=%s&lon=%s&appid=%s&units=metric" % (lat, long, api_key)
-        )
+        
+        url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={api_key}&units=metric"
 
         response = requests.get(url)
         data = json.loads(response.text)
 
         try:
-            temperature = (
-                "The current temperature in "
-                + data["timezone"]
-                + " is "
-                + str(data["current"]["temp"])
-                + " degrees Celsius."
-            )
-
-            temperature_tomorrow = (
-                "The temperature tomorrow will be "
-                + str(data["daily"][1]["temp"]["day"])
-                + " during the day and "
-                + str(data["daily"][1]["temp"]["night"])
-                + " during the night."
+            city = data.get("name", "your chosen location")
+            temperature = data["main"]["temp"]
+            description = data["weather"][0]["description"]
+            information = (
+                f"The current temperature in {city} is {temperature:.1f} degrees Celsius.\n"
+                f"Additional details: {description}."
             )
 
         except Exception:
-            temperature = "Could not retrieve weather data."
-            temperature_tomorrow = "Could not retrieve future weather data."
+            information = "Could not retrieve weather data."
 
-        window["temperature"].update(temperature)
-        window["temperature_tomorrow"].update(temperature_tomorrow)
+        window["weather_info"].update(information)
 
     # help the user get coordinates for their location
     if event == "Get Coordinates":
