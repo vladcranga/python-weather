1c4e06688231f8933586c7c74aab93c8df387d7f
Vlad Mihai Cranga <vmcranga1@sheffield.ac.uk>
2024-08-20 18:28:22 +0300

    add app icon

--- weather/main.py
+++ weather/main.py
@@ -3,8 +3,15 @@ from weather.gui import Weather
 import tkinter as tk
 
 def main():
+    # Create the main window
     root = tk.Tk()
     Weather(root)
+
+    # Weather icons created by iconixar - Flaticon
+    # https://www.flaticon.com/free-icons/weather
+    icon = tk.PhotoImage(file="icon.png")
+    root.iconphoto(True, icon)
+    
     root.mainloop()
 
 if __name__ == "__main__":
